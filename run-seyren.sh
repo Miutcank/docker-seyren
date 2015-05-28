a9d13ab172215d838b8147f250e6d36304127cce
Zsolt <zsolt@miutcank.hu>
2015-05-28 17:36:39 +0200

    adding option to inject MONGO_URL ENV var

--- run-seyren.sh
+++ run-seyren.sh
@@ -10,7 +10,10 @@ if [ -z ${GRAPHITE_URL} ]
        export GRAPHITE_URL=$1
 fi
 
-export MONGO_URL=mongodb://$MONGODB_PORT_27017_TCP_ADDR:$MONGODB_PORT_27017_TCP_PORT/seyren
+if [ -z ${MONGO_URL} ]
+   then
+       export MONGO_URL=mongodb://$MONGODB_PORT_27017_TCP_ADDR:$MONGODB_PORT_27017_TCP_PORT/seyren
+fi
 
 echo "Graphite URL $GRAPHITE_URL"
 echo "Mongo URL $MONGO_URL"
